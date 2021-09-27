f3e361f1f3e9eda9a994b512ea45df33c88ef4b1
taiprogrammer <taiza.marques@bandtec.com.br>
2021-09-27 17:33:53 -0300

    when from professor

--- HelloWorld/app/src/main/java/com/example/helloworld/KotlinBasicsFromProfessor.kt
+++ HelloWorld/app/src/main/java/com/example/helloworld/KotlinBasicsFromProfessor.kt
@@ -170,4 +170,26 @@ fun main() {
         is String -> println("$x is a String")
         else -> println("$x is none of the above")
     }
+
+    /**
+    var x : Any = 13.37
+    when(x) {
+    is Int -> println("$x is an Int")
+    !is Double -> println("$x is not Double")
+    is String -> println("$x is a String")
+    else -> println("$x is none of the above")
+    }
+     **/
+
+    val x2 : Any = 13.37
+//assign when to a variable
+    val result2 =  when(x2) {
+//let condition for each block be only a string
+        is Int -> "is an Int"
+        !is Double -> "is not Double"
+        is String -> "is a String"
+        else -> "is none of the above"
+    }
+//then print x with the result
+    print("$x2 $result2")
 }

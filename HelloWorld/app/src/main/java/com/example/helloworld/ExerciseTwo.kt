b3b34ed53518b402cd8551c2d6917e16d616092f
taiprogrammer <taiza.marques@bandtec.com.br>
2021-10-07 23:43:35 -0300

    Exercise creating variable and functions

--- HelloWorld/app/src/main/java/com/example/helloworld/ExerciseTwo.kt
+++ HelloWorld/app/src/main/java/com/example/helloworld/ExerciseTwo.kt
@@ -2,10 +2,32 @@ package com.example.helloworld
 
 fun main() {
     val mobileOne = MobilePhone("Android", "Xiaomi", "Redmi Note 8")
+    mobileOne.chargeBattery()
+    mobileOne.chargeBatteryFrom(30)
 }
 
 class MobilePhone(osName : String, brand : String, model : String) {
+    /* My solution */
+    var battery : Int = 50
+    var charged : Int = 10
+    var actualBattery : Int = battery + charged
+    /* Professor solution */
+    private var batteryFrom : Int = 30
+
     init {
         println("Cellphone: $osName, $brand and $model")
+    }
+
+    /* Professor solution */
+    fun chargeBatteryFrom(chargedBy:Int) {
+        println("Battery was $batteryFrom and is at ${batteryFrom + chargedBy} now")
+        batteryFrom += chargedBy
+    }
+
+    /* My solution */
+    fun chargeBattery() {
+        println("Battery before: $battery \n" +
+                "How much was charged: $charged \n" +
+                "My actual battery is: $actualBattery")
     }
 }
